27e4367da9a2c1524b139df1451a6fe3be33da40
BoniniF <filippomariabonini@libero.it>
2025-12-04 19:26:08 +0100

    Update script.js

--- api/script.js
+++ api/script.js
@@ -24,7 +24,7 @@ export default async function handler(req, res) {
             let links = extractLinks(html, url);
 
             // limita a max 10.000 caratteri (senza spezzare)
-            links = clampLinks(links, 10000);
+            links = clampLinks(links, 9600);
 
             result[continente][paese].push({
               source: url,
