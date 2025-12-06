1965ea5bcc69b943807af73c91be49e809b01286
BoniniF <filippomariabonini@libero.it>
2025-12-06 16:30:06 +0100

    Auto-f 2025-12-06 16:30:05

--- api/sendMail.js
+++ api/sendMail.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
 
     await transporter.sendMail({
       from: process.env.EMAIL_USER,
-      to: "TUO_INDIRIZZO_EMAIL",
+      to: "filippomariabonini@libero.it",
       subject: "Pagina Focus - Report Automatico",
       html
     });
